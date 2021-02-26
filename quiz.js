b5788ef0c107619fc5984ec10426ada2e15a0d91
JP Castrillon <jpcas001@gmail.com>
2021-02-26 14:17:38 -0500

    creating html template and javascript instructions

---
+++ quiz.js
@@ -0,0 +1,39 @@
+/* 
+  1. Store correct answers
+   - When quiz begins, no answers are correct
+*/
+
+let basketball = Michael Jordan 
+let golf = Tiger Woods 
+let football = Tom Brady 
+let tennis = Roger Federer 
+let football = Cristiano Ronaldo 
+
+// 2. Store the rank of a player
+
+
+// 3. Select the <main> HTML element
+
+
+/*
+  4. Ask at least 5 questions
+   - Store each answer in a variable
+   - Keep track of the number of correct answers
+*/
+
+prompt('who is the goat of soccer?')
+if (Cristiano Ronaldo) {
+console.log('siiiiiuuuuu!');
+}
+
+
+/*
+  5. Rank player based on number of correct answers
+   - 5 correct = Gold
+   - 3-4 correct = Silver
+   - 1-2 correct = Bronze
+   - 0 correct = No crown
+*/
+
+
+// 6. Output results to the <main> element
