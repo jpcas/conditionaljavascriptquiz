1618865e299f93a67bbb8d7d44684d0d156d1040
JP Castrillon <jpcas001@gmail.com>
2021-02-26 15:02:10 -0500

    writing conditional statements using if else

--- quiz.js
+++ quiz.js
@@ -2,8 +2,11 @@
   1. Store correct answers
    - When quiz begins, no answers are correct
 */
-
-
+const answer1 = 'string, number, undefined, null, boolean and symbol';
+const answer2 = 'netscape';
+const answer3 = 'strict operator';
+const answer4 = 'True';
+const answer5 = 'False';
 
 // 2. Store the rank of a player
 
@@ -17,10 +20,25 @@
    - Keep track of the number of correct answers
 */
 const questionOne = prompt('What are JavaScript Data types? ');
+    if (questionOne === 'string, number, undefined, null, boolean and symbol') {
+        alert('You are correct!')
+    } else alert('Wrong answer!');
 const questionTwo = prompt('Which company developed netscape?');
+    if (questionTwo === 'netscape') {
+        alert('You are correct!')
+    } else alert('Wrong Answer!');
 const questionThree = prompt('What is this === operator called?');
-const questionFour = prompt('What are all the types of Pop up boxes available in JavaScript?');
+    if (questionThree === 'strict operator') {
+        alert('You are right!');
+    } else alert('Wront Answer!')
+const questionFour = prompt('Prompts and alerts pop up boxes? True or False');
+if (questionFour === 'True') {
+    alert('You are right!');
+}else alert('WRONG!')
 const questionFive = prompt('Java and Javascript are the same. True or False');
+    if (questionFive === 'True') {
+        alert('You are right!')
+    } else alert('Wrong again!');
 
 
 
